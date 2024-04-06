71c006209479020053663d46ed9bf9e11180e921
johnhongannguyen <johhaneshongan@gmail.com>
2024-04-06 10:55:00 -0700

    implement play rule and user input

--- script.py
+++ script.py
@@ -33,3 +33,35 @@ def get_input():
             name = stacks[i].get_name()
             letter = choices[i]
             print(f"Enter {name} for {letter}")
+        user_input = input("")
+
+        if user_input in choices:
+            for i in range(len(stacks)):
+                return stacks[i]
+
+
+# Play the game
+num_user_moves = 0
+
+while right_stack.get_size() != num_disks:
+    print("\n\n\n...Current Stacks...")
+    for stack in stacks:
+        stack.print_items()
+    while True:
+        print("nWhich stack do you want to move from?\n")
+        from_stack = get_input()
+        print("\nWhich stack do you want to move to?\n")
+        to_stack = get_input()
+
+        if from_stack.get_size() == 0:
+            print("\n\nInvalid Move. Try Again")
+        elif to_stack.get_size() == 0 or from_stack.peek() < to_stack.peek():
+            disk = from_stack.pop()
+            to_stack.push(disk)
+            num_user_moves += 1
+            break
+        else:
+            print("\n\nInvalid Move. Try Again")
+
+print("\n\nYou completed the game in {0} moves, and the optimal number of moves is {1}".format(
+    num_user_moves, num_optimal_moves))
